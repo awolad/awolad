0fd0859daa482d4eb441c17c6c8e6076cc35e88f
Awolad Hossain <awolad1122@gmail.com>
2018-12-08 13:04:27 +0600

    deploy

---
+++ precache-manifest.4fc785c784c928de2887470b358b692f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "39164f8a10551ebaebe4",
+    "url": "/js/chunk-vendors.30c08d41.js"
+  },
+  {
+    "revision": "cb9d7cfed3cee988f2aa",
+    "url": "/js/app.ca8aace2.js"
+  },
+  {
+    "revision": "eedd24cf98f3eb6c32f2",
+    "url": "/js/about.49171c60.js"
+  },
+  {
+    "revision": "164c41b4e5e1e0915e00d580751a3124",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cb9d7cfed3cee988f2aa",
+    "url": "/css/app.d841ba09.css"
+  }
+];
